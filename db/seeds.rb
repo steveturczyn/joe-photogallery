e2c654cf21c1a4ab08103ea0d4bbc6389d8c57e9
Joe Levinger <jlevinger@jtonedesigns.com>
2014-09-08 11:53:56 -0400

    Modify Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ Image.delete_all
 Category.delete_all
 
 c1 = Category.create(name: "Wildlife")
-c2 = Category.create(name: "Architecture")
+c2 = Category.create(name: "Abstractions")
 c3 = Category.create(name: "Flowers")
 c4 = Category.create(name: "Nature")
 
 Image.create(title: "Florida Bird", location: "Fort Pierce, FL", description: "Beautiful bird, shot with a 400 mm zoom lens", image_link: File.open(Rails.root.join('public/tmp/bird.jpg')), category: c1)
-Image.create(title: "Opera House Detail", location: "Boston, MA", description: "Terrific chandelier", image_link: File.open(Rails.root.join('public/tmp/operahouse.jpg')), category: c2)
-Image.create(title: "Rhododendron", location: "Boston, MA", description: "Beautiful flower, shot with a macro lens", image_link: File.open(Rails.root.join('public/tmp/flower.jpg')), category: c3)
+Image.create(title: "Blue Sky", location: "Sleepy Hollow, NY", description: "Shot from a car at dusk", image_link: File.open(Rails.root.join('public/tmp/skyblue.jpg')), category: c2)
+Image.create(title: "Magnolia", location: "Boston, MA", description: "Beautiful flower, shot with a macro lens", image_link: File.open(Rails.root.join('public/tmp/magnolia.jpg')), category: c3)
 Image.create(title: "Ocean", location: "Fort Pierce, FL", description: "Atlantic Ocean, shot with a 10 mm ultra wide-ange lens", image_link: File.open(Rails.root.join('public/tmp/ocean.jpg')), category: c4)
