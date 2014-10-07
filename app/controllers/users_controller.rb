95fb0af0a3dff60a921f81fee79348d55710e7dc
Joe Levinger <jlevinger@jtonedesigns.com>
2014-10-07 14:09:33 -0400

    Refactor Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,26 @@
 class UsersController < ApplicationController
 
   def index
-    determine_categories
-    if params[:first_name].present?
-      @category = @categories.first
-    else
-      @category = Category.find_by(name: params[:cat], user_id: session[:user_id])
-    end
-
-    @pictures = @category.pictures
-    @picture = @pictures.find_by_represent_category(true)
+    @categories = Picture.select{|picture| picture.represent_category }.map{|p| p.category }.select{|category| category.user_id == session[:user_id] }.sort!{|a, b| a.name.downcase <=> b.name.downcase }
     
-    @prev_category = prev_category
-    @next_category = next_category
+    @category = Category.find_by(name: params[:cat], user_id: session[:user_id]) || @categories.first
+
+    @picture = Picture.select{|picture| picture.represent_category }.select{|p| @category.id == p.category_id }.first
+
+    prev_category
+    next_category
   end
 
   private
 
-  def determine_categories
-    pictures = Picture.all
-    if params[:first_name]
-      user = User.find_by(first_name: params[:first_name], last_name: params[:last_name])
-    else
-      user = User.find(session[:user_id])
-    end
-    id = user.id
-    user_categories = Category.select{|category| category.user_id == id }
-    user_categories_array = []
-    
-    user_categories.each do |category|
-      user_categories_array << category.id
-    end
-    
-    good_categories = pictures.map{|picture| picture.category_id}.uniq
-    good_user_categories = good_categories & user_categories_array
-    all_categories = Category.order(name: :asc)
-    @categories = []
-
-    all_categories.each_with_index do |category, index|  
-      @categories << all_categories[index] if good_user_categories.include?(all_categories[index].id)
-    end
-  end
-
   def prev_category
     category = @categories[@categories.find_index(@category)-1] || @categories.last
-    category.name
+    @prev_category = category.name
   end
 
   def next_category
     category = @categories[@categories.find_index(@category)+1] || @categories.first
-    category.name
+    @next_category = category.name
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,23 +4,26 @@ describe UsersController do
   describe 'GET index' do
     let!(:charlie) {Fabricate(:user, first_name: "Charlie", last_name: "Chan", id: 1)}
 
-    let!(:cherries) {Fabricate(:category, name: "Cherries", user: charlie, id: 1)}
-    let!(:bananas) {Fabricate(:category, name: "Bananas", user: charlie, id: 2)}
-    let!(:apples) {Fabricate(:category, name: "Apples", user: charlie, id: 3)}
+    let!(:cherries) {Fabricate(:category, name: "Cherries", user: charlie)}
+    let!(:bananas) {Fabricate(:category, name: "Bananas", user: charlie)}
+    let!(:apples) {Fabricate(:category, name: "Apples", user: charlie)}
     
-    let!(:bing) {Fabricate(:picture, title: "Bing", category: cherries, category_id: cherries.id)}
+    let!(:bing) {Fabricate(:picture, title: "Bing", category: cherries, category_id: cherries.id, represent_category: true)}
     let!(:chiquita) {Fabricate(:picture, title: "Chiquita", category: bananas, category_id: bananas.id, represent_category: true)}
-    let!(:mcintosh) {Fabricate(:picture, title: "McIntosh", category: apples, category_id: apples.id)}
+    let!(:mcintosh) {Fabricate(:picture, title: "McIntosh", category: apples, category_id: apples.id, represent_category: true)}
 
     it "builds a Categories collection" do
+      session[:user_id] = 1
       get :index, first_name: "Charlie", last_name: "Chan"
       expect(assigns(:categories).first).to eq(apples)
     end
     it "renders the index template" do
+      session[:user_id] = 1
       get :index, first_name: "Charlie", last_name: "Chan"
       expect(response).to render_template :index
     end
     it "selects the first category when User's first and last names are provided as params" do
+      session[:user_id] = 1
       get :index, first_name: "Charlie", last_name: "Chan"
       expect(assigns(:category)).to eq(apples)
     end
@@ -29,17 +32,13 @@ describe UsersController do
       get :index, cat: "Bananas", user_id: bananas.id
       expect(assigns(:category)).to eq(bananas)
     end
-    it "creates an array of Pictures objects from a Category object" do
-      session[:user_id] = 1
-      get :index, cat: "Bananas", user_id: bananas.id
-      expect(assigns(:pictures)).to eq([chiquita])
-    end
     it "creates a Picture object from an array of Pictures objects" do
       session[:user_id] = 1
       get :index, cat: "Bananas", user_id: bananas.id
       expect(assigns(:picture)).to eq(chiquita)
     end
     it "verifies that categories are in alpha order" do
+      session[:user_id] = 1
       get :index, first_name: "Charlie", last_name: "Chan"
       expect(assigns(:categories)).to eq [apples, bananas, cherries]
     end
